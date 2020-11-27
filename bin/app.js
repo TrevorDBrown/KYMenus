c8dc0311a0be1cf7caa5ef8a188a92820b899bf4
Trevor Brown <trevordylanbrown@gmail.com>
2020-11-27 16:28:41 -0600

    Create app.js/ts
    Initialized app.js/ts

---
+++ bin/app.js
@@ -0,0 +1,8 @@
+"use strict";
+/*
+    KYMenus
+    (c)2020 Trevor D. Brown. All rights reserved.
+    
+    app.ts - the entry point for the application.
+*/
+var clientAPI = require('./modules/api/client');
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+/*
+    KYMenus
+    (c)2020 Trevor D. Brown. All rights reserved.
+    
+    app.ts - the entry point for the application.
+*/
+
+const clientAPI = require('./modules/api/client');
