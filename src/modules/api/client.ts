23b7e7476ccd55f51c48c3d7a08fda450d4ffe22
Trevor D. Brown <trevordylanbrown@gmail.com>
2020-12-05 22:48:36 -0600

    Update client.ts
    - Began writing endpoints for API.

--- src/modules/api/client.ts
+++ src/modules/api/client.ts
@@ -8,6 +8,12 @@
 import express = require('express');
 const app: express.Application = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+import * as generalTypes from './../../types/generalTypes'
+import * as dbInterface from './../db/db';
+
 // / - the Base URL for the site. Making a GET request to this endpoint results in the homepage of the site being sent.
 app.get('/', (req, res) => {
     res.status(200).send("Hello world!");
@@ -19,7 +25,42 @@ app.get('/api/', (req, res) => {
 });
 
 app.get('/api/getAllRestaurants', (req, res) => {
-    res.status(200).send("/api/getAllRestaurants")
+    
+    dbInterface.executeQuery("GetAllRestaurants", (requestStatus, queryResults) => {
+        var response = {
+            "status": requestStatus,
+            "queryResponse": queryResults
+        };
+        
+        res.status(200).send(response);
+    });
+
+});
+
+app.get('/api/getMenusWithMenuItemsByRestaurant', (req, res) => {
+    
+    var requestStatus: generalTypes.Status;
+
+    if (!req.body.restaurantPublicID){
+        requestStatus = "Error";
+        var response =
+         {
+            "status": requestStatus,
+            "message": "Missing required field(s): restaurantPublicID"
+        }
+        res.status(400).send(response);
+    }else {
+        var input: string[] = [req.body.restaurantPublicID]
+        dbInterface.executeQuery("GetMenusWithMenuItemsByRestaurant", (requestStatus, queryResults) => {
+            var response = {
+                "status": requestStatus,
+                "queryResponse": queryResults
+            };
+            
+            res.status(200).send(response);
+        });
+    }
+
 });
 
 app.listen(3000, () => {
