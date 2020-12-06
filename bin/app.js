3725d93c518f8e8c3c0f7c1d5fc967c6e037717b
Trevor D. Brown <trevordylanbrown@gmail.com>
2020-12-05 22:44:17 -0600

    Delete bin directory
    Deleted, since this will be regenerated at compile.

--- bin/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/*
-    KYMenus
-    (c)2020 Trevor D. Brown. All rights reserved.
-    
-    app.ts - the entry point for the application.
-*/
-var clientAPI = require('./modules/api/client');
