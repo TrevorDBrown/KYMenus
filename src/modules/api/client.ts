4c670fffdf946190b8d8e4feb76b354de1884cb9
Trevor D. Brown <trevordylanbrown@gmail.com>
2020-12-26 16:12:33 -0600

    Update client.ts
    - Added references to queries to pull hours and menu data based on provided Restaurant Public ID.

--- src/modules/api/client.ts
+++ src/modules/api/client.ts
@@ -7,13 +7,13 @@
 
 import path = require('path');
 import express = require('express');
+import async = require('async');
+var dbInterface = require('./../db/db');
 import { QueryResult } from '../../types/databaseTypes';
-const app: express.Application = express();
 
+const app: express.Application = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
-
-var dbInterface = require('./../db/db');
 
 /*
     Static Routes (for resources)
@@ -76,29 +76,79 @@ app.post('/api/getRestaurantByPublicID', (req, res) => {
     var requestStatus: string = "";
 
     if (!req.body.restaurantPublicID){
-        requestStatus = "Error";
-        var response =
-         {
-            "status": requestStatus,
-            "message": "Missing required field(s): restaurantPublicID"
-        }
+        let response: {status: string; error: string;} = {
+            status: "Error",
+            error: "Missing required field(s): restaurantPublicID"
+        };
+
         res.status(400).send(response);
     }else {
-        var input: [{field: string; value: string;}] = [{
-            field: "RestaurantPublicID",
-            value: req.body.restaurantPublicID
-        }];
-        
-        dbInterface.executeQuery("GetRestaurantByRestaurantPublicID", (requestStatus: string, queryResults: QueryResult, error: Error) => {
-            var response = {
-                "status": requestStatus,
-                "queryResponse": queryResults,
-                "error": error
-            };
-            
-            res.status(200).send(response);
-        },
-        input);
+        let response: {status: string; queryResponse: QueryResult[]; error: string};
+        var input: [{field: string; value: string;}];
+
+        async.series([
+            function (callback) {
+                input = [{
+                    field: "RestaurantPublicID",
+                    value: req.body.restaurantPublicID
+                }];
+
+                dbInterface.executeQuery("GetRestaurantByRestaurantPublicID", (requestStatus: string, queryResults: QueryResult, error: Error) => {
+                    if (!error){
+                        response = {
+                            status: "Success",
+                            queryResponse: [queryResults],
+                            error: ""
+                        }
+                        
+                        callback(null);
+                    }else{
+                        callback(error);
+                    }
+                },
+                input);
+            },
+            function (callback) {
+                input = [{
+                    field: "RestaurantPublicID",
+                    value: req.body.restaurantPublicID
+                }];
+
+                dbInterface.executeQuery("GetHoursByRestaurantPublicID", (requestStatus: string, queryResults: QueryResult, error: Error) => {
+                    if (!error){
+                        response.queryResponse.push(queryResults);
+                        callback(null);
+                    }else{
+                        callback(error);
+                    }
+                },
+                input);
+            },
+            function (callback) {
+                input = [{
+                    field: "RestaurantPublicID",
+                    value: req.body.restaurantPublicID
+                }];
+
+                dbInterface.executeQuery("GetMenusWithMenuItemsByRestaurantPublicID", (requestStatus: string, queryResults: QueryResult, error: Error) => {
+                    if (!error){
+                        response.queryResponse.push(queryResults);
+                        callback(null);
+                    }else{
+                        callback(error);
+                    }
+                },
+                input);
+            }
+        ], function(error){
+            if (!error) {
+                res.status(200).send(response);
+            }else{
+                response["status"] = "Error";
+                response["error"] = error.message;
+                res.status(500).send(response);
+            }
+        });
     }
 
 });
