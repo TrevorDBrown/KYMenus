5353d0b33ddf0f24f74b4495271f1a934224e937
Trevor D. Brown <trevordylanbrown@gmail.com>
2020-12-26 16:14:06 -0600

    Update restaurant-instance.js
    - Added code to reference new data for restaurants (menu items, hours, etc.) and display them on page.
    - Added code to refit the page footer based on content size (needs adjustment, as a condition could occur when the page body is shorter than screen height, but footer is still fixed.)

--- src/ui/client/js/restaurant-instance.js
+++ src/ui/client/js/restaurant-instance.js
@@ -1,10 +1,21 @@
 $(() => {
     getRestaurantData("0b535283-db14-4cde-8e36-33a72c8cc37e").then(function(restaurantData){
         if (restaurantData){
-            populatePageFields(restaurantData["queryResponse"]["queryResults"][0]);
+            populatePageFields(restaurantData["queryResponse"]);
         }
+    }).then(function(){
+        refitFooter();
     });    
 });
+
+function refitFooter(){
+    var footerElement = $("#kym-footer");
+
+    if (footerElement.prev().height() >= screen.height){
+        console.log("Body exceeds screen height.");
+        footerElement.removeClass("footer-fixed");
+    }
+}
 
 function getRestaurantData(restaurantPublicID){
     return $.ajax({
@@ -18,14 +29,76 @@ function getRestaurantData(restaurantPublicID){
     });
 }
 
-function populatePageFields(restaurantData){
+function populatePageFields(allRestaurantData){
     var restaurantNameElement = $("#kym-restaurant-name");
     var restaurantAddressElement = $("#kym-restaurant-address");
+    var restaurantHoursElement = $("#kym-restaurant-hours");
+    var restaurantMenuElement = $("#kym-restaurant-menu");
 
-    restaurantNameElement.text(restaurantData["restaurant_name"]);
+    var basicRestaurantData = allRestaurantData[0].queryResults[0];
+    var restaurantHoursData = allRestaurantData[1].queryResults;
+    var restaurantMenuData = allRestaurantData[2].queryResults;
 
-    restaurantAddressElement.append(restaurantData["address"]);
+    $(document).prop("title", basicRestaurantData["restaurant_name"] + " - KYMenus");
+
+    restaurantNameElement.text(basicRestaurantData["restaurant_name"]);
+
+    restaurantAddressElement.append(basicRestaurantData["address"]);
     restaurantAddressElement.append($("<br />"));
-    restaurantAddressElement.append(restaurantData["city_name"] + ", " + restaurantData["state_name"] + " " + restaurantData["zip_code"]);
+    restaurantAddressElement.append(basicRestaurantData["city_name"] + ", " + basicRestaurantData["state_name"] + " " + basicRestaurantData["zip_code"]);
+
+    restaurantHoursElement.text("Hours");
+    restaurantHoursElement.append($("<br />"));
+    
+    restaurantHoursData.forEach(dataElement => {
+        restaurantHoursElement.append($("<br />"));
+        restaurantHoursElement.append(dataElement["day_of_week"] + ":&emsp;" + dataElement["OpenTime"] + " - " + dataElement["CloseTime"]);
+    });
+
+    var restaurantCategoryElementTemplate = $("<div>", {
+        "class": "card"
+    });
+
+    var restaurantCategoryElements = [];
+
+    restaurantMenuData.forEach(dataElement => {
+        var elementExists = false;
+
+        restaurantCategoryElements.forEach(element => {
+            if (element.attr("data-category") == dataElement["MenuSection"]){
+                elementExists = true;
+                
+                var existingRestaurantCategoryBody = element.find(".card-body");
+
+                existingRestaurantCategoryBody.append($("<br />"));
+                existingRestaurantCategoryBody.append(dataElement["Name"] + ":&emsp;" + dataElement["Price"]);
+            }
+        });
+
+        if (!elementExists){
+            var newRestaurantCategoryElement = restaurantCategoryElementTemplate.clone();
+            var newRestaurantCategoryElementHeader = $("<div>", {
+                "class": "card-header"
+            });
+
+            var newRestaurantCategoryElementBody = $("<div>", {
+                "class": "card-body"
+            });
+
+            newRestaurantCategoryElement.attr("data-category", dataElement.MenuSection);
+            newRestaurantCategoryElementHeader.text(dataElement["MenuSection"]);
+            newRestaurantCategoryElementBody.append(dataElement["Name"] + ":&emsp;" + dataElement["Price"]);
+            
+            newRestaurantCategoryElement.append(newRestaurantCategoryElementHeader);
+            newRestaurantCategoryElement.append(newRestaurantCategoryElementBody);
+
+            restaurantCategoryElements.push(newRestaurantCategoryElement);
+            
+        }
+    });
+
+    restaurantCategoryElements.forEach(element => {
+        restaurantMenuElement.append(element);
+    });
 
 }
