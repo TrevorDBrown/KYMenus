e25674313db8df3d95ac81f97f90d6a7096d1f81
Trevor D. Brown <trevordylanbrown@gmail.com>
2021-01-16 21:50:13 -0600

    Create restaurants.js
    - Added comment block to top of file.

---
+++ src/ui/client/js/restaurants.js
@@ -0,0 +1,7 @@
+/*
+    KYMenus
+    (c)2020-2021 Trevor D. Brown. All rights reserved.
+    
+    restaurant.js - JavaScript for handling functions and interface changes to the /restaurants/ page.
+*/
+
