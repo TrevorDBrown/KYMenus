ac2824510af38b64c60d904a634ab6723baf7077
Trevor D. Brown <trevordylanbrown@gmail.com>
2021-01-27 22:18:25 -0600

    Update client.ts
    - Moved all functions toward top of file, with all Express routes and language at bottom of file.
    - Maybe will break out these functions into a separate file.

--- src/modules/api/client.ts
+++ src/modules/api/client.ts
@@ -13,156 +13,13 @@ var dbModule = require('./../db/db');
 var uiModule = require('./../ui/ui');
 import * as geolocationModule from './../geo/geolocation';
 
-import { QueryResult } from '../../types/databaseTypes';
+import { QueryResult } from '../../types/database-types';
 
 const app: express.Application = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
-/*
-    Static Routes (for resources)
-*/
-app.use('/shared/bootstrap/', express.static(path.join(__dirname, './../../../node_modules/bootstrap/dist/')));
-app.use('/shared/jquery/', express.static(path.join(__dirname, './../../../node_modules/jquery/dist/')));
-app.use('/shared/fontawesome/css/', express.static(path.join(__dirname, './../../../node_modules/@fortawesome/fontawesome-free/css/')));
-app.use('/shared/fontawesome/webfonts/', express.static(path.join(__dirname, './../../../node_modules/@fortawesome/fontawesome-free/webfonts/')));
-
-// KYMenus Client Interface specific routes
-app.use('/css/', express.static(path.join(__dirname, './../../ui/client/css/')));
-app.use('/js/', express.static(path.join(__dirname, './../../ui/client/js/')));
-app.use('/images/', express.static(path.join(__dirname, './../../private/assets/user-profile/')));
-
-/*
-    KYMenus Client Interface
-*/
-// / - the Base URL for the site. Making a GET request to this endpoint results in the homepage of the site being sent.
-app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, './../../ui/client/index.html'));
-});
-
-// /restaurants/:PublicRestaurantID - displays page for specified restaurant. Otherwise, if invalid, redirects to /restaurants.
-app.get('/where-to-eat', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, './../../ui/client/where-to-eat.html'));
-});
-
-app.get('/restaurants', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, './../../ui/client/restaurants.html'));
-});
-
-app.get('/restaurants/:RestaurantUniqueID', (req, res) => {
-    getRestaurantData(req.params.RestaurantUniqueID, (status: string, queryResponse: QueryResult[], error: string) => { 
-        if (status == "Success"){
-            var responseFromGenerateRestaurantPage: {status?: string, error?: string, html?: string} = uiModule.generateRestaurantPage(queryResponse);
-    
-            if (responseFromGenerateRestaurantPage.html){
-                res.status(200).send(responseFromGenerateRestaurantPage.html);
-            }else{
-                res.status(404).send("File not found.");
-            }
-        }else{
-            res.status(404).send("File not found.");
-        }
-    });
-
-});
-
-/* 
-    KYMenus API
-*/
-// api - the API Base URL. Making a GET request to this endpoint results in a listing of all public API endpoints.
-app.get('/api/', (req, res) => {
-    res.status(200).send("Successful request.");
-});
-
-app.get('/api/geolocationTest', (req, res) => {
-    if (req.query.latitude && req.query.longitude) {
-        getZIPCodeFromGeolocation(<string>req.query.latitude, <string>req.query.longitude, (zipCode) => {
-            res.status(200).send(zipCode);
-        });
-    }else{
-        res.status(400).send("Malformed request.");
-    }
-});
-
-app.get('/api/getAllRestaurants', (req, res) => {
-
-    dbModule.executeQuery("GetAllRestaurants", (requestStatus: string, queryResults: QueryResult, error: Error) => {
-        var response = {
-            "status": requestStatus,
-            "queryResponse": queryResults,
-            "error": error
-        };
-        
-        res.status(200).send(response);
-    });
-
-});
-
-app.post('/api/getRestaurantByPublicID', (req, res) => {
-
-    var requestStatus: string = "";
-
-    if (!req.body.restaurantPublicID){
-        let response: {status: string; error: string;} = {
-            status: "Error",
-            error: "Missing required field(s): restaurantPublicID"
-        };
-
-        res.status(400).send(response);
-    }else {
-        getRestaurantData(req.body.restaurantPublicID, (status: string, queryResponse: QueryResult[], error: string) => {
-            let response: {status: string, queryResponse: QueryResult[], error: string} = {
-                status: status,
-                queryResponse: queryResponse,
-                error: error
-            };
-
-            if (status == "Success"){
-                res.status(200).send(response);
-            }else{
-                res.status(500).send(response);
-            }
-        });
-    }
-
-});
-
-app.get('/api/getMenusWithMenuItemsByRestaurant', (req, res) => {
-    
-    var requestStatus: string = "";
-
-    if (!req.body.restaurantPublicID){
-        requestStatus = "Error";
-        var response =
-         {
-            "status": requestStatus,
-            "message": "Missing required field(s): restaurantPublicID"
-        }
-        res.status(400).send(response);
-    }else {
-        var input: [{field: string; value: string;}] = [{
-            field: "RestaurantPublicID",
-            value: req.body.restaurantPublicID
-        }];
-        
-        dbModule.executeQuery("GetMenusWithMenuItemsByRestaurant", (requestStatus: string, queryResults: QueryResult, error: Error) => {
-            var response = {
-                "status": requestStatus,
-                "queryResponse": queryResults,
-                "error": error
-            };
-            
-            res.status(200).send(response);
-        },
-        input);
-    }
-
-});
-
-app.listen(3000, () => {
-    console.log("KYMenus is running on port 3000.");
-});
-
+/* Internal Functions */
 function getZIPCodeFromGeolocation(latitude: string, longitude: string, callback: (zipCode: string) => void): void {
     geolocationModule.convertGeolocationToZIP(latitude, longitude, (zipCode) => {
         return callback(zipCode);
@@ -311,3 +168,143 @@ function getRestaurantData(restaurantUniqueID: string, callback: (status: string
     });
 
 }
+
+/* Static Routes (for resources) */
+app.use('/shared/bootstrap/', express.static(path.join(__dirname, './../../../node_modules/bootstrap/dist/')));
+app.use('/shared/jquery/', express.static(path.join(__dirname, './../../../node_modules/jquery/dist/')));
+app.use('/shared/fontawesome/css/', express.static(path.join(__dirname, './../../../node_modules/@fortawesome/fontawesome-free/css/')));
+app.use('/shared/fontawesome/webfonts/', express.static(path.join(__dirname, './../../../node_modules/@fortawesome/fontawesome-free/webfonts/')));
+
+// KYMenus Client Interface specific routes
+app.use('/css/', express.static(path.join(__dirname, './../../ui/client/css/')));
+app.use('/js/', express.static(path.join(__dirname, './../../ui/client/js/')));
+app.use('/images/', express.static(path.join(__dirname, './../../private/assets/user-profile/')));
+
+/* KYMenus Client Interface */
+// / - the Base URL for the site. Making a GET request to this endpoint results in the homepage of the site being sent.
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, './../../ui/client/index.html'));
+});
+
+// /restaurants/:PublicRestaurantID - displays page for specified restaurant. Otherwise, if invalid, redirects to /restaurants.
+app.get('/where-to-eat', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, './../../ui/client/where-to-eat.html'));
+});
+
+app.get('/restaurants', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, './../../ui/client/restaurants.html'));
+});
+
+app.get('/restaurants/:RestaurantUniqueID', (req, res) => {
+    getRestaurantData(req.params.RestaurantUniqueID, (status: string, queryResponse: QueryResult[], error: string) => { 
+        if (status == "Success"){
+            var responseFromGenerateRestaurantPage: {status?: string, error?: string, html?: string} = uiModule.generateRestaurantPage(queryResponse);
+    
+            if (responseFromGenerateRestaurantPage.html){
+                res.status(200).send(responseFromGenerateRestaurantPage.html);
+            }else{
+                res.status(404).send("File not found.");
+            }
+        }else{
+            res.status(404).send("File not found.");
+        }
+    });
+
+});
+
+/* 
+    KYMenus API
+*/
+// api - the API Base URL. Making a GET request to this endpoint results in a listing of all public API endpoints.
+app.get('/api/', (req, res) => {
+    res.status(200).send("Successful request.");
+});
+
+app.get('/api/geolocationTest', (req, res) => {
+    if (req.query.latitude && req.query.longitude) {
+        getZIPCodeFromGeolocation(<string>req.query.latitude, <string>req.query.longitude, (zipCode) => {
+            res.status(200).send(zipCode);
+        });
+    }else{
+        res.status(400).send("Malformed request.");
+    }
+});
+
+app.get('/api/getAllRestaurants', (req, res) => {
+
+    dbModule.executeQuery("GetAllRestaurants", (requestStatus: string, queryResults: QueryResult, error: Error) => {
+        var response = {
+            "status": requestStatus,
+            "queryResponse": queryResults,
+            "error": error
+        };
+        
+        res.status(200).send(response);
+    });
+
+});
+
+app.post('/api/getRestaurantByPublicID', (req, res) => {
+
+    var requestStatus: string = "";
+
+    if (!req.body.restaurantPublicID){
+        let response: {status: string; error: string;} = {
+            status: "Error",
+            error: "Missing required field(s): restaurantPublicID"
+        };
+
+        res.status(400).send(response);
+    }else {
+        getRestaurantData(req.body.restaurantPublicID, (status: string, queryResponse: QueryResult[], error: string) => {
+            let response: {status: string, queryResponse: QueryResult[], error: string} = {
+                status: status,
+                queryResponse: queryResponse,
+                error: error
+            };
+
+            if (status == "Success"){
+                res.status(200).send(response);
+            }else{
+                res.status(500).send(response);
+            }
+        });
+    }
+
+});
+
+app.get('/api/getMenusWithMenuItemsByRestaurant', (req, res) => {
+    
+    var requestStatus: string = "";
+
+    if (!req.body.restaurantPublicID){
+        requestStatus = "Error";
+        var response =
+         {
+            "status": requestStatus,
+            "message": "Missing required field(s): restaurantPublicID"
+        }
+        res.status(400).send(response);
+    }else {
+        var input: [{field: string; value: string;}] = [{
+            field: "RestaurantPublicID",
+            value: req.body.restaurantPublicID
+        }];
+        
+        dbModule.executeQuery("GetMenusWithMenuItemsByRestaurant", (requestStatus: string, queryResults: QueryResult, error: Error) => {
+            var response = {
+                "status": requestStatus,
+                "queryResponse": queryResults,
+                "error": error
+            };
+            
+            res.status(200).send(response);
+        },
+        input);
+    }
+
+});
+
+app.listen(3000, () => {
+    console.log("KYMenus is running on port 3000.");
+});
