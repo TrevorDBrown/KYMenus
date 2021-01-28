8cba9c69d6f98585dab504a5fd66176514514a07
Trevor D. Brown <trevordylanbrown@gmail.com>
2021-01-27 22:20:13 -0600

    Update restaurants.js
    - Added call to geolocation function.

--- src/ui/client/js/restaurants.js
+++ src/ui/client/js/restaurants.js
@@ -5,3 +5,10 @@
     restaurant.js - JavaScript for handling functions and interface changes to the /restaurants/ page.
 */
 
+$(() => {
+    // Fetch Geolocation Data
+    getGeolocationCoordinates().then((geolocationData) => {
+        console.log(geolocationData);
+    });
+
+});
